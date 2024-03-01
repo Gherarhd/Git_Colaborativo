1a09a64ff4b15780c032af4d41e03d4be0a52739
Gherarhd Ravelo <ravelogherarhd@gmail.com>
2024-03-01 01:46:31 -0300

    - Organización de la carpeta assets \ - Adición de nuevas imágenes \ - Mejoras en el archivo CSS \ - Incorporación de una nueva librería en el archivo JavaScript

---
+++ assets/main.js
@@ -0,0 +1,27 @@
+
+window.swiper = new Swiper('.slider__contenedor', {
+    slidesPerView: 1,
+    slideClass: 'slider__slide',
+    createElements: true,
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+    pagination: true,
+    navigation: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
+   
+  
+  });
+
+
+
+
+
+   
+   
+  
